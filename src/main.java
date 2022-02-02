465648bec0e91118f4b865687b91ea4131d689a5
EmmanuelPedraza <pedraemm001@warren.k12.in.us>
2022-02-02 11:30:40 -0800

    class and object assigment # 1

---
+++ src/main.java
@@ -0,0 +1,8 @@
+public class main {
+    public static void main(String[] args) {
+        Rect rect1 = new Rect (2, 4);
+
+        System.out.println("the length of rect1 is: " + rect1.getLength());
+       System.out.println("The width of rect1 is: " + rect1.getWidth());
+    }
+}
